5e6fe735d1fcbfcd8a2cddb75e9d70be2c7d1494
Eunbyeol Cho <202104x@gmail.com>
2025-05-09 18:48:58 +0900

    useState 복습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import NameInput from "./NameInput";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <NameInput />
+    </div>
+  );
 };
 
 export default App;
